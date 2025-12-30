383e617dbba80689487c90186ef40fea7385f6d3
Giulex84 <giulianom.gm@gmail.com>
2025-12-30 15:46:52 +0100

    Create vite.config.ts

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,11 @@
+// apps/web/vite.config.ts
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: "es2019",
+  },
+});
