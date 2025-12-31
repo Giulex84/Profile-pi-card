f05538e3eb356d5ef42f373b21768be70d9be861
Giulex84 <giulianom.gm@gmail.com>
2025-12-31 02:53:02 +0100

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+Enumerating objects: 42, done.
+remote: Counting objects: 100% (42/42), done.
+remote: Compressing objects: 100% (34/34), done.
+remote: Total 34 (delta 18), reused 0 (delta 0), pack-reused 0 (from 0)
+Unpacking objects: 100% (34/34), 7.52 KiB | 550.00 KiB/s, done.
+From https://github.com/Giulex84/Profile-pi-card
+ * branch            main       -> FETCH_HEAD
+   2bf68b0..65043c5  main       -> origin/main
+Updating 2bf68b0..65043c5
+Fast-forward
+ apps/web/index.html            |   9 +-
+ apps/web/src/App.tsx           | 159 +++++++++------------------------
+ apps/web/src/PublicProfile.tsx |  13 +++
+ apps/web/src/main.tsx          |  14 +--
+ apps/web/vite.config.ts        |   5 --
+ 5 files changed, 59 insertions(+), 141 deletions(-)
+ create mode 100644 apps/web/src/PublicProfile.tsx
+~/.../apps/web $
