eecd4751f547ebbde965ec8cd9eef7c4e6213041
Giulex84 <giulianom.gm@gmail.com>
2025-12-31 02:43:43 +0100

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,6 @@
-// apps/web/vite.config.ts
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "es2019",
-  },
 });
