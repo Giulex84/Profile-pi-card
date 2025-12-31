6a44a5b2743a630cdfc79f1e01300d6ddfbdb34e
Giulex84 <giulianom.gm@gmail.com>
2025-12-31 02:42:36 +0100

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,18 +1,8 @@
-
-
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Root container not found");
-}
-
-const root = createRoot(container);
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
