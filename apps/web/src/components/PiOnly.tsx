3d9dbf97a8ec22f3cba67fc35811b460978a411f
Giulex84 <giulianom.gm@gmail.com>
2026-01-01 22:49:36 +0100

    Create PiOnly.tsx

---
+++ apps/web/src/components/PiOnly.tsx
@@ -0,0 +1,8 @@
+export default function PiOnly() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40vh" }}>
+      <h2>Pi Browser Required</h2>
+      <p>This utility works exclusively inside the Pi Browser.</p>
+    </div>
+  );
+}
